e4e7561dd2641575e0fa4827a635c05bddb03cf5
zhouzhengyi <1062871517@qq.com>
2021-03-06 17:17:23 +0800

    Initial commit

---
+++ mybatis-01/src/main/java/com/jie/dao/UserDao.java
@@ -0,0 +1,18 @@
+package com.jie.dao;
+
+import com.jie.pojo.User;
+
+import java.util.List;
+
+/**
+ * ClassName:UserDao
+ * Package:com.jie.dao
+ * Description:每一个牛B的人物都有一段苦B的岁月，只要像傻B一样坚持，终将会牛B！
+ *
+ * @date:2021/3/6 15:06
+ * @author:VX438364246
+ */
+public interface UserDao {
+     List<User> getUserList();
+
+}
---
+++ mybatis-01/src/main/java/com/jie/pojo/User.java
@@ -0,0 +1,57 @@
+package com.jie.pojo;
+
+/**
+ * ClassName:User
+ * Package:com.jie.pojo
+ * Description:每一个牛B的人物都有一段苦B的岁月，只要像傻B一样坚持，终将会牛B！
+ *
+ * @date:2021/3/6 14:59
+ * @author:VX438364246
+ */
+public class User {
+    private int id;
+    private String name;
+    private  String pwd;
+
+    public User() {
+
+    }
+    public User(int id,String name,String pwd) {
+        this.id=id;
+        this.name=name;
+        this.pwd=pwd;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+}
